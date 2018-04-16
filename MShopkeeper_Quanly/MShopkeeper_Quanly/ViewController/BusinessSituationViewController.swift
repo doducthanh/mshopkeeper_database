c1bd3c3dc8adb2b9858cef86c1884d3d2646577f
ddthanh <doducthanh95@gmail.com>
2018-04-16 22:37:59 +0700

    chinh file do thi

--- MShopkeeper_Quanly/MShopkeeper_Quanly/ViewController/BusinessSituationViewController.swift
+++ MShopkeeper_Quanly/MShopkeeper_Quanly/ViewController/BusinessSituationViewController.swift
@@ -18,52 +18,92 @@ class BusinessSituationViewController: UIViewController {
     @IBOutlet var viewChartTrend: UIView!
     @IBOutlet var vChartRevenue: UIView!
     @IBOutlet var vChartTrends: UIView!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        //ConfigChart()
-    }
-
-    override func viewDidAppear(_ animated: Bool) {
-        ConfigChart()
-        drawChartTrend()
-    }
+    @IBOutlet var viewChart: UIView!
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    fileprivate func ConfigChart() {
-        let chartViewModel = ChartViewModel.init()
-        let chart: Chart = chartViewModel.barsChart(frame: self.vChartRevenue.bounds)
+    fileprivate var chart: Chart?
+    
+    let sideSelectorHeight: CGFloat = 80
+    
+    fileprivate func barsChart(horizontal: Bool) -> Chart {
+        let tuplesXY = [(2, 8), (4, 9), (6, 10), (8, 12), (12, 17)]
         
-        self.viewChartRevenue.addSubview(chart.view)
-        self.chartView = chart
+        func reverseTuples(_ tuples: [(Int, Int)]) -> [(Int, Int)] {
+            return tuples.map{($0.1, $0.0)}
+        }
         
+        let chartPoints = (horizontal ? reverseTuples(tuplesXY) : tuplesXY).map{ChartPoint(x: ChartAxisValueInt($0.0), y: ChartAxisValueInt($0.1))}
+        
+        let labelSettings = ChartLabelSettings(font: ExamplesDefaults.labelFont)
+        
+        let generator = ChartAxisGeneratorMultiplier(2)
+        let labelsGenerator = ChartAxisLabelsGeneratorFunc {scalar in
+            return ChartAxisLabel(text: "\(scalar)", settings: labelSettings)
+        }
+        let xGenerator = ChartAxisGeneratorMultiplier(2)
+        
+        let xModel = ChartAxisModel(firstModelValue: 0, lastModelValue: 20, axisTitleLabels: [ChartAxisLabel(text: "Axis title", settings: labelSettings)], axisValuesGenerator: xGenerator, labelsGenerator: labelsGenerator)
+        let yModel = ChartAxisModel(firstModelValue: 0, lastModelValue: 20, axisTitleLabels: [ChartAxisLabel(text: "Axis title", settings: labelSettings.defaultVertical())], axisValuesGenerator: generator, labelsGenerator: labelsGenerator)
+        
+        let barViewGenerator = {(chartPointModel: ChartPointLayerModel, layer: ChartPointsViewsLayer, chart: Chart) -> UIView? in
+            let bottomLeft = layer.modelLocToScreenLoc(x: 0, y: 0)
+            
+            let barWidth: CGFloat = 30
+            
+            let settings = ChartBarViewSettings(animDuration: 0.5)
+            
+            let (p1, p2): (CGPoint, CGPoint) = {
+                return (CGPoint(x: chartPointModel.screenLoc.x, y: bottomLeft.y), CGPoint(x: chartPointModel.screenLoc.x, y: chartPointModel.screenLoc.y))
+            }()
+            return ChartPointViewBar(p1: p1, p2: p2, width: barWidth, bgColor: UIColor.blue.withAlphaComponent(0.6), settings: settings)
+        }
+        
+        let frame = ExamplesDefaults.chartFrame(viewChart.bounds)
+        let chartFrame = chart?.frame ?? CGRect(x: frame.origin.x, y: frame.origin.y, width: frame.size.width, height: frame.size.height - sideSelectorHeight)
+        
+        let chartSettings = ExamplesDefaults.chartSettingsWithPanZoom
+        
+        let coordsSpace = ChartCoordsSpaceLeftBottomSingleAxis(chartSettings: chartSettings, chartFrame: chartFrame, xModel: xModel, yModel: yModel)
+        let (xAxisLayer, yAxisLayer, innerFrame) = (coordsSpace.xAxisLayer, coordsSpace.yAxisLayer, coordsSpace.chartInnerFrame)
+        
+        let chartPointsLayer = ChartPointsViewsLayer(xAxis: xAxisLayer.axis, yAxis: yAxisLayer.axis, chartPoints: chartPoints, viewGenerator: barViewGenerator)
+        
+        let settings = ChartGuideLinesDottedLayerSettings(linesColor: UIColor.black, linesWidth: ExamplesDefaults.guidelinesWidth)
+        let guidelinesLayer = ChartGuideLinesDottedLayer(xAxisLayer: xAxisLayer, yAxisLayer: yAxisLayer, settings: settings)
+        
+        return Chart(
+            frame: chartFrame,
+            innerFrame: innerFrame,
+            settings: chartSettings,
+            layers: [
+                xAxisLayer,
+                yAxisLayer,
+                guidelinesLayer,
+                chartPointsLayer
+            ]
+        )
     }
     
-    fileprivate func drawChartTrend() {
-//        let chartConfig
-        let chartConfig = ChartConfigXY.init(xAxisConfig: ChartAxisConfig.init(from: 0, to: 12, by: 2),
-                                             yAxisConfig: ChartAxisConfig.init(from: 0, to: 10, by: 2))
-        let chart = LineChart.init(frame: self.vChartTrends.frame,
-                                   chartConfig: chartConfig,
-                                   xTitle: "",
-                                   yTitle: "",
-                                   line: (chartPoints: [(1, 10), (5, 5), (7, 1), (8, 4)], color: UIColor.red))
-        self.viewChartTrend.addSubview(chart.view)
-        self.chartLine = chart
+    fileprivate func showChart(horizontal: Bool) {
+        self.chart?.clearView()
+        
+        let chart = barsChart(horizontal: horizontal)
+        viewChart.addSubview(chart.view)
+        self.chart = chart
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    override func viewDidLoad() {
+        showChart(horizontal: false)
+//        if let chart = chart {
+//            let sideSelector = DirSelector(frame: CGRect(x: 0, y: chart.frame.origin.y + chart.frame.size.height, width: view.frame.size.width, height: sideSelectorHeight), controller: self)
+//            view.addSubview(sideSelector)
+//        }
     }
-    */
+    
     
     @IBAction func onClickMenu(_ sender: Any) {
         openLeft()
